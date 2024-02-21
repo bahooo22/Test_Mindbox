a4c4df0d051337c3dbb3c88c34bf739aee8577cf
Ivan Zelenkevich <a7706061@outlook.com>
2024-02-21 22:36:41 +0300

    Добавление файла с SQL-запросом из ТЗ

---
+++ Query.sql
@@ -0,0 +1,4 @@
+﻿SELECT p.product_name, COALESCE(c.category_name, 'Без категории') AS category_name
+FROM products p
+LEFT JOIN product_categories pc ON p.product_id = pc.product_id
+LEFT JOIN categories c ON pc.category_id = c.category_id
